2cabb2d2ff5c1b4ef6456676715dc24a785156dd
a-hamzah <Spellbreaker907@gmail.com>
2024-05-05 19:41:20 +0500

    h0_3 calculated

--- rtb/SCARAV3/kinematics/forward_kinematics.py
+++ rtb/SCARAV3/kinematics/forward_kinematics.py
@@ -1,7 +1,9 @@
 import numpy as np
 import math
 
-""" Delaring our variables in mm unit"""
+""" Declaring our variables in mm unit"""
+
+""" Parameters: a1 = 10, a2 = 10, a3 = 5, a4 = 10, d1 = 5, theta2 = 30 (Deg), theta3 = -45 (Deg)"""
 
 # Link Lenghts
 
@@ -9,3 +11,56 @@ a1 = float(input("a1: "))
 a2 = float(input("a2: "))
 a3 = float(input("a3: "))
 a4 = float(input("a4: "))
+
+# Joint variables (prismatic = mm) (revolute = degrees)
+
+d1 = float(input("d1: "))
+theta2 = float(input("theta2: "))
+theta3 = float(input("theta3: "))
+
+# Degree to radian conversion using numpy
+
+theta2 = np.deg2rad(theta2)
+theta3 = np.deg2rad(theta3)
+
+# For convenience
+theta0 = np.deg2rad(0)
+
+# Parametric table (DH-Table) (theta, alpha, r, d)
+
+PT = [[theta0, theta0, 0, a1 + d1],
+               [theta2, theta0, a2, 0],
+               [theta3, theta0, a4, a3],]
+
+# Homogenous TF Matrix Formula
+
+""" Parametric table has 3 rows, so we will have 3 homogeous TF matrices """
+
+H_list = []
+
+for i in range(len(PT)):
+    H = [[np.cos(PT[i][0]), -np.sin(PT[i][0]) * np.cos(PT[i][1]), np.sin(PT[i][0]) * np.sin(PT[i][1]), PT[i][2] * np.cos(PT[i][0])],
+        [np.sin(PT[i][0]), np.cos(PT[i][0]) * np.cos(PT[i][1]), -np.cos(PT[i][0]) * np.sin(PT[i][1]), PT[i][2] * np.sin(PT[i][0])],
+        [0, np.sin(PT[i][1]), np.cos(PT[i][1]), PT[i][3]],
+        [0, 0, 0, 1]]
+    
+    H_list.append(H)
+    
+# Printing the list of TF matrices
+
+for i, H in enumerate(H_list):
+    print(f'H{i}_{i+1} =\n', np.matrix(H))
+
+""" Finding out the final result H0_3 (Moving from 0th frame to 3rd frame) """
+
+# Step1: Finding H0_2 = H0_1 * H1_2
+
+H0_2 = np.dot(H_list[0], H_list[1])
+
+# Step2: Finding H0_3 = H0_2 * H2_3
+
+H0_3 = np.dot(H0_2, H_list[2])
+
+print(f'H0_3 =\n {np.around(H0_3, 3)}')
+
+
--- rtb/SCARAV3/numpy_test.py
+++ rtb/SCARAV3/numpy_test.py
@@ -6,3 +6,20 @@ radians = np.deg2rad(degrees)
 
 print("Degrees:", degrees)
 print("Radians:", radians)
+
+zero_mat = np.zeros((4, 4))
+print(zero_mat)
+print(zero_mat[1,:])
+mat_size = zero_mat.shape
+print(mat_size)
+
+random_array = np.random.rand(4, 4)
+arr_size = random_array.shape
+
+print(arr_size[0])
+
+PT = [[1, 3, 0, 7],
+               [4, 0, 2, 0],
+               [9, 5, 4, 3],]
+print(PT)
+
